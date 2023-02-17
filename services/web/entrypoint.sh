2e7e03359512c087c98bd7b5b342bc123cb2b133
roman <roman@home>
2023-02-17 10:58:57 +0300

    win.yml и override.yml для запуска в windows

--- services/web/entrypoint.sh
+++ services/web/entrypoint.sh
@@ -2,4 +2,7 @@
 
 groupmod --gid $USER_GID www-data  && \
 usermod --uid $USER_ID www-data; \
+curl -o /var/www/html/bx-site/bitrixsetup.php -L http://www.1c-bitrix.ru/download/scripts/bitrixsetup.php;  \
+curl -o /var/www/html/bx-site/restore.php -L https://www.1c-bitrix.ru/download/files/scripts/restore.php;  \
+chown www-data:www-data /var/www/html/bx-site/bitrixsetup.php /var/www/html/bx-site/restore.php
 exec docker-php-entrypoint apache2-foreground
